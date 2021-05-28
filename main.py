de716fccae15a474c1c51c333684f4693c1a9845
Abdullahi1120 <>
2021-05-28 18:11:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+s = 'hijklmrnopqrrrstuvyxz' # The first question of strings 
+print(s[-1])
+
+print(len(s))   # The second question of strings
+
+print(s[5:20])  # From m to x
+
+print(s[::-1])   # The entire string in reverse
+
+print(('hijklmrnopqrrrstuvyxz').count('r')) # prints the number of r in the string
+
+print(s.find('q'))   # Location of q
+
+print(s[2])   # The third element.
+
+
+
+  # For Loops
+for i in range(10 + 1):  # prints 1 to 10
+    print(i)
+
+for i in range(20 ** 1):  # prints the code 10 times
+  print('I love python')
+
+
+  # While loops      # prints 1 to 10
+  i = 1
+  while(i<=10):
+     print(i)
+     i += 1
+
+
+  i = 1
+  while i <= 20:
+    print("I love python")
+    i += 1
+
+
